eaa3842d3d0461dcfd7cf0f7ef07671de95984d6
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-02-27 14:12:03 +0000

    message

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main
    public static void main(String[] args)
    {
        Word w1 = new Word("Sri Lanka");
+       System.out.println(w1.toString());
+       System.out.println(w1.length());
+       System.out.println(w1.substring(3));
+       System.out.println(w1.vowelCount());
+       System.out.println(w1.backwards());
+       System.out.println(w1.insertLetter('E', 3));
        
    }
 }
--- Word.java
+++ Word.java
@@ -17,6 +17,42 @@ public class Word
       theWord = w;
    }
 
+   public int length() {
+      return theWord.length();
+   }
+
+   public String substring(int x) {
+      return theWord.substring(x);
+   }
+
+   public String substring (int x, int y) {
+      return theWord.substring(x, y);
+   }
+
+   public int vowelCount(){
+      int count = 0;
+      String word2 = theWord.toLowerCase();
+      for(int i = 0; i < word2.length(); i++) {
+         if(word2.substring(i, i +1).equals("a") || word2.substring(i, i +1).equals("e")  || word2.substring(i, i +1).equals("i") || word2.substring(i, i +1).equals("o") || word2.substring(i, i +1).equals("u")) {
+            count = count + 1;
+         }
+      }
+      return count;
+   }
+
+   public String backwards() {
+      String back = "";
+      for(int i = theWord.length() ; i > 0; i--) {
+         back = back + theWord.substring(i -1, i);
+      }
+      return back;
+   }
+
+   public String insertLetter(char c, int i) {
+      String full = theWord.substring(0, i) + c + theWord.substring(i);
+      return full;
+   }
+
    public String toString()
    {
       return theWord;
